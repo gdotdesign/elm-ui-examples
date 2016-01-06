721bab374fad48c478b870dd5f4fd24770fd3824
Gusztáv Szikszai <guszti5@hotmail.com>
2016-01-06 13:59:00 +0100

    Cleanup FolderView.

--- source/VideoLibrary/Components/FolderView.elm
+++ source/VideoLibrary/Components/FolderView.elm
@@ -1,7 +1,6 @@
 module VideoLibrary.Components.FolderView where
 
 import Ext.Signal exposing ((>>>))
-
 import Html exposing (node)
 
 import VideoLibrary.Components.Item as Item
@@ -21,45 +20,45 @@ type Action
   = FolderAction Int Item.Action
   | VideoAction Int Item.Action
 
-
 init : List Folder -> List Video -> Model
 init folders videos =
   { videos = List.map (Item.init "video") videos
-  , folders = List.map (Item.init "folder") folders
-              |> List.filter (\item -> item.id /= 0)
-
+  , folders =
+      List.map (Item.init "folder") folders
+      |> List.filter (\item -> item.id /= 0)
   }
+
+updatedItem : Int -> Item.Action -> Item.Model -> Item.Model
+updatedItem id act item =
+  if item.id == id then
+    Item.update act item
+  else
+    item
 
 update: Action -> Model -> Model
 update action model =
   case action of
     FolderAction id act ->
-      let
-        updatedFolders view =
-          if view.id == id then
-            Item.update act view
-          else
-            view
-      in
-        { model | folders = List.map updatedFolders model.folders }
+      { model | folders = List.map (updatedItem id act) model.folders }
     VideoAction id act ->
-      let
-        updatedVideos view =
-          if view.id == id then
-            Item.update act view
-          else
-            view
-      in
-        { model | videos = List.map updatedVideos model.videos }
+      { model | videos = List.map (updatedItem id act) model.videos }
 
-view: Signal.Address Action -> ViewModel -> Model -> Html.Html
+view : Signal.Address Action -> ViewModel -> Model -> Html.Html
 view address viewModel model =
-  node "video-library-folder" []
-    ((List.map (renderItem address (viewModel.folderActions) FolderAction) model.folders)
-      ++
-      (List.map (renderItem address (viewModel.videoActions) VideoAction) model.videos)
-    )
+  let
+    folders =
+      renderItems address viewModel.folderActions FolderAction model.folders
+    videos =
+      renderItems address viewModel.videoActions VideoAction model.videos
+  in
+    node "video-library-folder" [] (folders ++ videos)
 
+renderItems : Signal.Address Action -> (Item.Model -> Item.ViewModel)
+            -> (Int -> Item.Action -> Action) -> List Item.Model -> List Html.Html
+renderItems address actions action items =
+  List.map (renderItem address actions action) items
+
+handleClick : Bool -> Model -> Model
 handleClick pressed model =
   { model | folders = List.map (Item.handleClick pressed) model.folders
           , videos = List.map (Item.handleClick pressed) model.videos
